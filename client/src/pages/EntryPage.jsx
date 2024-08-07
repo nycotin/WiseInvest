fb94a7970aa6ad75d190e70e814e94afed0c324b
nycotin <ntincani@hotmail.it>
2024-08-07 17:56:10 +0200

    Changed href to navigate in EntryPage

--- client/src/pages/EntryPage.jsx
+++ client/src/pages/EntryPage.jsx
@@ -1,15 +1,19 @@
 import AppLogo from '../assets/logo-no-bg.png';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+
 import '../App.css';
 
 function EntryPage () {
+    const navigate = useNavigate();
+
     return (
         <div className="entry-page">
             <h1>WiseInvest</h1>
             <img src={AppLogo} alt="Logo" />
             <div className="buttons">
-                <Button variant="secondary" href="/login">Login</Button>
-                <Button variant="secondary" href="/register">Register</Button>
+                <Button variant="secondary" onClick={() => navigate('/login')}>Login</Button>
+                <Button variant="secondary" onClick={() => navigate('/register')}>Register</Button>
             </div>
         </div>
     )
