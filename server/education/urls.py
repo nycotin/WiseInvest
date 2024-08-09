c0d08be6d45da3c7939176575980834c69605333
nycotin <ntincani@hotmail.it>
2024-08-09 18:40:05 +0200

    Added seeding view and seeded education DB

--- server/education/urls.py
+++ server/education/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-    
+    path('', views.index, name='index'),
+
+
+    path('seed_db', views.seed_db, name='seed_db')
 ]
