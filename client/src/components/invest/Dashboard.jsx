8508b1a390511c795018ea8c0f417d971894e849
nycotin <ntincani@hotmail.it>
2024-10-07 22:29:36 +0200

    Fixed invest dashboard

--- client/src/components/invest/Dashboard.jsx
+++ client/src/components/invest/Dashboard.jsx
@@ -9,101 +9,96 @@ import Table from 'react-bootstrap/Table';
 import '../../App.css';
 import '../../index.css';
 
-import liveStocks from '../../stockStaticData.json'
-
 
 function Dashboard() {
   const [stocks, setStocks] = useState([]);
-  const [userStocks, setUserStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {  
     const dashboard = document.querySelector('.dashboard');
     dashboard.style.display = 'block';
 
-    //? view disabled due to api call limits
-    // function getLiveStocks(){
-    //     axios.get('/invest/get-live-stocks')
-    //     .then(response => {
-    //         setStocks(response.data.stocks);
-    //     });
-    // }
-    setStocks(liveStocks.stocks)
+    function getStocks(){
+        axios.get('/invest/get-stocks')
+        .then(response => {
+            setStocks(response.data.stocks);
+        });
+    }
 
-    function getUserStocks(){
-      axios.get('/invest/get-user-stocks')
+    function getPortfolio(){
+      axios.get('/invest/get-portfolio')
       .then(response => {
-        if(response.data.user_stocks){
-          setUserStocks(response.data.user_stocks);
+        if(response.data.portfolio_data){
+          setPortfolio(response.data.portfolio_data);
         } else {
-          setUserStocks([]);
+          setPortfolio([]);
         }
       });
     }
 
-    //? Activate line for production
-    // getLiveStocks();
-    getUserStocks();
-  }, [])
-
-
-  function checkCurrentValue(obj){
-    let updatedStock = stocks.filter(item => item.stock === obj.stock_symbol)[0]
-
-    if (obj.price_on_purchase < updatedStock.extracted_price){
-      return true
-    } else if (obj.price_on_purchase > updatedStock.extracted_price){
-      return false
-    } else {
-      return "stable"
+    function getTransactionHistory(){
+      axios.get('/invest/get-transactions')
+      .then(response => {
+        if(response.data.transactions_history){
+          setTransactions(response.data.transactions_history);
+        } else {
+          setTransactions([]);
+        }
+      });
     }
 
+    getStocks();
+    getPortfolio();
+    getTransactionHistory();
+  }, [])
+
+  function calculateGrowth(inv, val){
+    const data = {
+      "movement": 0,
+      "percent": 0,
+      "dir": ""
+    }
+
+    if (val > inv) {
+      data["movement"] = `+${(val - inv).toFixed(2)}`;
+      data["dir"] = "up";
+      data["percent"] = `+${((val - inv) / inv).toFixed(2)}%`;
+    } else if (inv > val) {
+      data["movement"] = (val - inv).toFixed(2);
+      data["dir"] = "down";
+      data["percent"] = `${((val - inv) / inv).toFixed(2)}%`;
+    } else {
+      data["movement"] = 0;
+      data["dir"] = "stable";
+      data["percent"] = 0;
+    }
+
+    return data;
   }
 
-  function countIncreased(){
-    let count = 0;
+  function filterStocks(str) {
+    const list = portfolio.filter(i => calculateGrowth(i.total_investment, i.total_value).dir === str)
 
-    userStocks.forEach(i => {
-      if (checkCurrentValue(i)){
-        count += 1;
-      }
-    })
-
-    return count;
+    return list;
   }
 
-  function countDecreased(){
-    let count = 0;
+  function getStock(sym){
+    const lookup = stocks.filter(stock => stock.symbol === sym);
+    const current_stock = lookup[0];
 
-    userStocks.forEach(i => {
-      if (!checkCurrentValue(i)){
-        count += 1;
-      }
-    })
-
-    return count;
+    return current_stock;
   }
-
-  function countStable(){
-    let count = 0;
-
-    userStocks.forEach(i => {
-      if (checkCurrentValue(i) === 'stable'){
-        count += 1;
-      }
-    })
-
-    return count;
-  }
-
+  console.log(transactions)
   return (
     <>
         <div className="dashboard" style={{ display: 'block' }}>
             <h2>Dashboard</h2>
             <div className="invest-dashboard">
               <Card className="user-stocks">
-                <Card.Title>My Stocks <sub><Badge bg="secondary">{userStocks.length}</Badge></sub></Card.Title>
-                { userStocks.length !== 0 ?
-                  <Table striped bordered hover>
+                <Card.Title>My Stocks</Card.Title>
+                { portfolio.length !== 0 ?
+                  <Table striped bordered>
                     <thead>
                       <tr>
                         <th>Symbol</th>
@@ -112,33 +107,52 @@ function Dashboard() {
                       </tr>
                     </thead>
                     <tbody>
-                      { userStocks.map(i => <tr key={i.stock_symbol}>
+                      { portfolio.map(i => <tr key={i.stock_symbol}>
                         <td>{i.stock_symbol}</td>
-                        <td><Link to={i.link} target="_blank">{i.stock_name}</Link></td>
+                        <td><Link to={getStock(i.stock_symbol).symbol} target="_blank">{getStock(i.stock_symbol).company_name}</Link></td>
                         <td>{i.quantity}</td>
                       </tr>) }
                     </tbody>
                   </Table>
-                  : 'No stocks purchased.'}
+                  : 'No stocks.' }
               </Card>
-              <Card className="increased-since-purchase">
-                <Card.Title>Increased <sub><Badge bg="primary">{ countIncreased() }</Badge></sub></Card.Title>
-                { userStocks.map(i => { if(checkCurrentValue(i)){
-                    return <li key={i.stock_symbol}><Link to={i.link} target="_blank">{i.stock_name}</Link></li>
-                }}) }
-              </Card>
-              <Card className="decreased-since-purchase">
-                <Card.Title>Decreased <sub><Badge bg="success">{ countDecreased() }</Badge></sub></Card.Title>
-                { userStocks.map(i => { if(!checkCurrentValue(i)){
-                    return <li key={i.stock_symbol}><Link to={i.link} target="_blank">{i.stock_name}</Link></li>
-                }}) }
-              </Card>
-              <Card className="stable-since-purchase">
-                <Card.Title>Stable <sub><Badge bg="warning">{ countStable() }</Badge></sub></Card.Title>
-                { userStocks.map(i => { if(checkCurrentValue(i) === 'stable'){
-                    return <li key={i.stock_symbol}><Link to={i.link} target="_blank">{i.stock_name}</Link></li>
-                }}) }
-              </Card>
+              <div className="stats">
+                <Card className="increased-since-purchase">
+                  <Card.Title>Increased <sub><Badge bg="primary">{ filterStocks('up').length }</Badge></sub></Card.Title>
+                  { filterStocks('up').length !== 0 ? filterStocks('up').map(i => <li key={i.stock_symbol}>{getStock(i.stock_symbol).company_name}</li>) : 'No stocks.' }
+                </Card>
+                <Card className="decreased-since-purchase">
+                  <Card.Title>Decreased <sub><Badge bg="success">{ filterStocks('down').length }</Badge></sub></Card.Title>
+                  { filterStocks('down').length !== 0 ? filterStocks('down').map(i => <li key={i.stock_symbol}>{getStock(i.stock_symbol).company_name}</li>) : 'No stocks.' }
+                </Card>
+                <Card className="stable-since-purchase">
+                  <Card.Title>Stable <sub><Badge bg="warning">{ filterStocks('stable').length }</Badge></sub></Card.Title>
+                  { filterStocks('stable').length !== 0 ? filterStocks('stable').map(i => <li key={i.stock_symbol}>{getStock(i.stock_symbol).company_name}</li>) : 'No stocks.' }
+                </Card>
+              </div>
+            <Card className="transactions">
+              <Card.Title>Transaction History</Card.Title>
+              <Table striped bordered>
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Stock symbol</th>
+                    <th>Company name</th>
+                    <th>Price on purchase</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { transactions.map(i => <tr key={i.id}>
+                    <td>{i.purchased_on}</td>
+                    <td>{i.stock_id}</td>
+                    <td>{getStock(i.stock_id).company_name}</td>
+                    <td>{i.price_on_purchase}</td>
+                    <td>{i.quantity}</td>
+                  </tr>) }
+                </tbody>
+              </Table>
+            </Card>
             </div>
         </div>
     </>
